872e07884f13f1e54dc1068fe595a64bdcd8ce9b
deww <deww@dewws-Mac-Pro.local>
2023-04-10 22:15:53 +0700

    first commit

---
+++ init.sql
@@ -0,0 +1,16 @@
+create user 'auth_user'@'localhost' identified by 'Qwerty@123';
+
+create database auth;
+
+grant all privileges on auth.* to 'auth_user'@'localhost';
+
+use auth;
+
+create table user(
+    id int not null auto_increment primary key,
+    email varchar(255) not null,
+    password varchar(255) not null
+);
+
+
+insert into user(email, password) values('klein@gmail.com', 'admin123');
